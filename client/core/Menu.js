a9fbbd2448359eaae13688b4cae37617a8a431c7
Kavinda Senarathne <kavindasenarathne94@gmail.com>
2018-10-30 02:24:58 +0530

    License updated

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -18,7 +18,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography type="title" color="inherit">
-        MERN Skeleton
+        ShopTalk
       </Typography>
       <Link to="/">
         <IconButton aria-label="Home" style={isActive(history, "/")}>
